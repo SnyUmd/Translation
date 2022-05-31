f07b8959385a4d6fbec022f302a25545e01d496e
SNY-AC\Acer <shinya.umd@gmail.com>
2022-06-01 01:39:32 +0900

    no message

--- Translation_app/Translation_app/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Translation_app/Translation_app/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace Translation_app {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Translation_app;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Translation_app;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
